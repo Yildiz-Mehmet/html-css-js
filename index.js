aa72e5fc07af45ffcdcaa2b06a4aaaba7669589e
yıldız <mesel4461@gmail.com>
2023-05-04 10:51:42 +0300

    as

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-function reverseString(number) {
-  let end = 1;
-  for (let i = number; i >= 1; i--) {
-    end *= i;
+// * Bir dizi içinde sadece tek sayıları içinden alıp başka bir dizi oluşturulmak isteniyor.Ve bu sayıların toplamını bulan bir fonksiyon yazılması isteniyor...
+
+sumOfOddNumbers = (array) => {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 1) {
+      sum += array[i];
+    }
   }
-  console.log(end);
-}
-reverseString(3);
+  return sum;
+};
+
+number = [1, 2, 6, 7, 3, 1, 23, 3];
+sum = sumOfOddNumbers(number);
+
+const x = 1;
+console.log("x= " + x);
