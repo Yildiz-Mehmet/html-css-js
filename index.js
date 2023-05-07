a7f35d66423f83adaef34df51ec13356c2398902
yıldız <mesel4461@gmail.com>
2023-05-08 01:26:33 +0300

    mkn

--- index.js
+++ index.js
@@ -1,50 +1,67 @@
-function aritmetik(arr) {
-  sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  return sum / arr.length;
-}
-// console.log(aritmetik([3, 4, 5, 4, 4]));
-function geometrik(arr) {
-  multible = 1;
-  for (let i = 0; i < arr.length; i++) {
-    multible *= arr[i];
-  }
-  return multible ** (1 / arr.length);
-}
-// console.log(geometrik([4, 4]));
-let length = [];
-function longestWord(arr) {
-  let splited = arr.split(" ");
-  for (let i = 0; i < splited.length; i++) {
-    length.push(splited[i].length);
-  }
-  let max = Math.max.apply(null, length);
-  let maxİndex = length.indexOf(max);
-  return splited[maxİndex];
-}
-console.log(
-  longestWord(
-    "merhaba nasılsın ben mehmet on tane relevetions nasılolurdabnmqadnwlkn"
-  )
-);
-let newarr = [];
+// function aritmetik(arr) {
+//   sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum / arr.length;
+// }
+// // console.log(aritmetik([3, 4, 5, 4, 4]));
+// function geometrik(arr) {
+//   multible = 1;
+//   for (let i = 0; i < arr.length; i++) {
+//     multible *= arr[i];
+//   }
+//   return multible ** (1 / arr.length);
+// }
+// // console.log(geometrik([4, 4]));
+// let length = [];
+// function longestWord(arr) {
+//   let splited = arr.split(" ");
+//   for (let i = 0; i < splited.length; i++) {
+//     length.push(splited[i].length);
+//   }
+//   let max = Math.max.apply(null, length);
+//   let maxİndex = length.indexOf(max);
+//   return splited[maxİndex];
+// }
+// console.log(
+//   longestWord(
+//     "merhaba nasılsın ben mehmet on tane relevetions nasılolurdabnmqadnwlkn"
+//   )
+// );
+// let newarr = [];
 
-function capitalizeWords(arr) {
-  let capital = arr.toUpperCase();
-  let splited = capital.split(" ");
+// function capitalizeWords(arr) {
+//   let capital = arr.toUpperCase();
+//   let splited = capital.split(" ");
 
-  for (let i = 0; i < splited.length; i++) {
-    for (let k = 0; k < splited[i].length; k++) {
-      if (k === 0) {
-        newarr += splited[i][k];
-      } else {
-        newarr += splited[i][k].toLowerCase();
-      }
-    }
-    newarr += " ";
+//   for (let i = 0; i < splited.length; i++) {
+//     for (let k = 0; k < splited[i].length; k++) {
+//       if (k === 0) {
+//         newarr += splited[i][k];
+//       } else {
+//         newarr += splited[i][k].toLowerCase();
+//       }
+//     }
+//     newarr += " ";
+//   }
+//   console.log(newarr.split(","));
+// }
+// console.log(capitalizeWords("ben iyiyim nasılsın evet aynen"));
+
+function arraySum(arr) {
+  let sum = 0;
+  let sorted = arr.sort(function (a, b) {
+    return a - b;
+  });
+  for (let i = 0; i < sorted.length - 1; i++) {
+    sum += sorted[i];
   }
-  console.log(newarr.split(","));
+
+  if (sum === sorted[sorted.length - 1]) {
+    return true;
+  } else {
+    return false;
+  }
 }
-console.log(capitalizeWords("ben iyiyim nasılsın evet aynen"));
+console.log(arraySum([1, 2, 4, 29, 22]));
