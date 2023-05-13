ba8e444dc889bc9511597addf7aed3598a15f41e
yıldız <mesel4461@gmail.com>
2023-05-13 22:02:49 +0300

    as

--- index1.js
+++ index1.js
@@ -1,1 +1,9 @@
+import axios from "axios";
 
+const cars = async () => {
+  const { data } = await axios.get(
+    "https://api.car2db.com/api/auto/v1/type.getAll.csv.en"
+  );
+  console.log(data);
+};
+cars();
