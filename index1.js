1f62c817135bbbe979d0a4dadd73b0ae0186d999
yıldız <mesel4461@gmail.com>
2023-05-14 21:41:55 +0300

    as

--- index1.js
+++ index1.js
@@ -1,11 +1,17 @@
-const cars = async () => {
-  try {
-    const { data } = await fetch(
-      "https://api.car2db.com/api/auto/v1/type.getDateUpdate.timestamp.en"
-    );
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-};
-cars();
+// const cars = async () => {
+//   const response = await fetch(
+//     " https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getModel&model=11459"
+//   );
+//   const jsonData = await response.json();
+
+//   console.log(jsonData);
+// };
+// cars();
+function Person(name, age, gender) {
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
+}
+
+let myPerson = new Person("John", 30, "male");
+console.log(myPerson);
